814d21724a557db61cfe195568ba1eb260183db8
Mr. Fluffy <git@sixearsm>
2022-10-26 08:04:25 +0000

    add flake

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  description = "Prelude replacement, external packages only, incl. tests";
+
+  inputs = {
+    nixpkgs.url     = "github:nixos/nixpkgs/be44bf67"; # nixos-22.05 2022-10-15
+    build-utils.url = "github:sixears/flake-build-utils/r0.0.0.0";
+    base0.url       = "github:sixears/base0/r0.0.4.0";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, build-utils, base0 }:
+    build-utils.lib.hOutputs self nixpkgs "base0t" { inherit base0; };
+}
