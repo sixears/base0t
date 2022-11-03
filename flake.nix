eeebf3c9347170e9abd039ad2782c086f4fa3646
Mr. Fluffy <git@sixearsm>
2022-11-03 06:22:02 +0000

    flake-build-utils->1.0.0.5

--- flake.nix
+++ flake.nix
@@ -3,8 +3,8 @@
 
   inputs = {
     nixpkgs.url     = "github:nixos/nixpkgs/be44bf67"; # nixos-22.05 2022-10-15
-    build-utils.url = "github:sixears/flake-build-utils/r1.0.0.3";
-    base0.url       = "github:sixears/base0/r0.0.4.1";
+    build-utils.url = "github:sixears/flake-build-utils/r1.0.0.5";
+    base0.url       = "github:sixears/base0/r0.0.4.2";
   };
 
   outputs = { self, nixpkgs, flake-utils, build-utils, base0 }:
