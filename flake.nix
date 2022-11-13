461e6c48c6ed06dc402f8d91b03b3cc68812821b
Mr. Fluffy <git@sixearsm>
2022-11-13 06:55:15 +0000

    fix fixed-package-name typo in flake-build-utils

--- flake.nix
+++ flake.nix
@@ -3,8 +3,8 @@
 
   inputs = {
     nixpkgs.url     = "github:nixos/nixpkgs/be44bf67"; # nixos-22.05 2022-10-15
-    build-utils.url = "github:sixears/flake-build-utils/r1.0.0.6";
-    base0.url       = "github:sixears/base0/r0.0.4.4";
+    build-utils.url = "github:sixears/flake-build-utils/r1.0.0.10";
+    base0.url       = "github:sixears/base0/r0.0.4.5";
   };
 
   outputs = { self, nixpkgs, build-utils, base0 }:
